dadf74dad95e971eab8570d60a74dd8e4124eabc
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2025-09-15 04:59:43 +0600

    55-3 Create a client-side post and Insert single-user data

---
+++ Module 55 Know more about node js, express js, MongoDB, and CRUD operation/Client-server/src/Components/Users.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Users = () => {
+    const handleAddUsr = e => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const newUser = { name, email };
+        console.log(newUser);
+
+        //create user in the db
+        fetch('http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newUser),
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log("data after creating user in the db", data);
+            })
+    }
+    return (
+        <div>
+            {/* add user */}
+            <div>
+                <form onSubmit={handleAddUsr}>
+                    <input type="text" name='name' />
+                    <br />
+                    <input type="email" name='email' />
+                    <br />
+                    <input type="submit" value="Add User" />
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Users;
--- Module 55 Know more about node js, express js, MongoDB, and CRUD operation/server/index2.js
+++ Module 55 Know more about node js, express js, MongoDB, and CRUD operation/server/index2.js
@@ -3,18 +3,18 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-
 // middleware
 app.use(cors());
 app.use(express.json());
 
+//Mongodb
+// User: Mezbahul
+// Pass: 2A3NW9ZuLLtGXaGu
 
-// user: simpleDBUser
-// pass: tiHNUs27sSTqVK3U
 
 // const uri = 'mongodb://127.0.0.1:27017';
-
 const uri = "mongodb+srv://mezbahul:2A3NW9ZuLLtGXaGu@cluster0.1jlx3rd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,45 +26,45 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    try{
+    try {
         await client.connect();
 
         const usersCollection = client.db('usersdb').collection('users');
 
+
         app.post('/users', async(req, res) =>{
+            console.log("data in the server",req.body)
             const newUser = req.body;
-            console.log("data in the server",req.body);
             const result = await usersCollection.insertOne(newUser);
             res.send(result);
         })
 
+        // app.post('/users',async (req,res)=>{
+        //     const user1= req.body;
+        //     console.log("data in the server",user1);
+        // })
+
+
+        // Connect the client to the server	(optional starting in v4.7)
         
-
-        await client.db('admin').command({ping: 1})
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }
-    finally{
-
+        // Send a ping to confirm a successful connection
+        //await client.db("admin").command({ ping: 1 });
+        //console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
     }
 }
 
-run().catch(console.dir)
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Simple Crud Server running')
-});
-
-app.listen(port, () => {
-    console.log(`Simple CRUD server running on, ${port}`)
+    res.send('Hello World!')
 })
 
-// try{
 
-// }
-// catch {
-    
-// }
-// finally{
-    
-// }
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
