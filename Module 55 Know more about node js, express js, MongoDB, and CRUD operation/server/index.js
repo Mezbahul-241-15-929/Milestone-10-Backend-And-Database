2167d720d1717d83082e5d4d6931e3a6d300eab7
Mezbahul-241-15-929 <islam241-15-929@diu.edu.bd>
2025-09-15 03:10:07 +0600

    55-2 Async Await, Try Catch and Connect to the database

--- Module 55 Know more about node js, express js, MongoDB, and CRUD operation/server/index.js
+++ Module 55 Know more about node js, express js, MongoDB, and CRUD operation/server/index.js
@@ -2,18 +2,50 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
+app.use(express.json())
 
 app.use(cors())
 
+//Mongodb
+// User: Mezbahul
+// Pass: 2A3NW9ZuLLtGXaGu
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mezbahul:2A3NW9ZuLLtGXaGu@cluster0.1jlx3rd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.get('/data', (req, res) => {
-  res.send('this is data...')
+    res.send('this is data...')
 })
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
